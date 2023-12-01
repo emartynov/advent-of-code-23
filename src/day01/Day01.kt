6f36265c121e441952aa796b8f4a0bb1b0215af2
Eugen Martynov <eugenemartynov@gmail.com>
2023-12-01 11:50:29 +0100

    Fix day 1 second part

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -12,10 +12,10 @@ fun main() {
         return input
             .asSequence()
             .map { calibrationValue ->
-                val firstDigitIndex = calibrationValue.indexOfFirst { it.isDigit() }
-                val lastDigitIndex = calibrationValue.indexOfLast { it.isDigit() }
-                "${calibrationValue[firstDigitIndex]}${calibrationValue[lastDigitIndex]}"
-            }
+            val firstDigitIndex = calibrationValue.indexOfFirst { it.isDigit() }
+            val lastDigitIndex = calibrationValue.indexOfLast { it.isDigit() }
+            "${calibrationValue[firstDigitIndex]}${calibrationValue[lastDigitIndex]}"
+        }
             .map { it.toInt() }
             .sum()
     }
@@ -23,20 +23,31 @@ fun main() {
     fun part2(input: List<String>): Int {
         return input
             .asSequence()
-            .map {
-                var result = it
-                wordDigits.forEachIndexed { index, wordAsDigit ->
-                    val digitString = (index + 1).toString()
-                    result = result.replace(wordAsDigit, digitString)
-                }
-                result
-            }
             .map { calibrationValue ->
-                val firstDigitIndex = calibrationValue.indexOfFirst { it.isDigit() }
-                val lastDigitIndex = calibrationValue.indexOfLast { it.isDigit() }
-                "${calibrationValue[firstDigitIndex]}${calibrationValue[lastDigitIndex]}"
+                val firstDigitWord = wordDigits
+                    .mapIndexed { index, digitWord ->
+                        calibrationValue.indexOf(digitWord) to (index + 1)
+                    }
+                    .filter { it.first > -1 }
+                    .minByOrNull { it.first }
+                val lastDigitWord = wordDigits
+                    .mapIndexed { index, digitWord ->
+                        calibrationValue.lastIndexOf(digitWord) to (index + 1)
+                    }
+                    .filter { it.first > -1 }
+                    .maxByOrNull { it.first }
+                val firstDigit = with(calibrationValue) {
+                    val index = indexOfFirst { it.isDigit() }
+                    if (index > -1) (index to substring(index, index + 1).toInt()) else null
+                }
+                val lastDigit = with(calibrationValue) {
+                    val index = indexOfLast { it.isDigit() }
+                    if (index > -1) (index to substring(index, index + 1).toInt()) else null
+                }
+                val digitPairs = listOfNotNull(firstDigitWord, lastDigitWord, firstDigit, lastDigit)
+                    .sortedBy { it.first }
+                10 * digitPairs.first().second + digitPairs.last().second
             }
-            .map { it.toInt() }
             .sum()
     }
 
