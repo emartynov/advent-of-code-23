5f3022468be8c4f635c3df71d860c7b3c5af610b
Eugen Martynov <eugenemartynov@gmail.com>
2023-12-19 16:21:49 +0100

    Prepare Day 20

---
+++ src/day20/Day20.kt
@@ -0,0 +1,23 @@
+package day20
+
+import println
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("day20/Day20_test")
+    check(part1(testInput) == 19114)
+    check(part2(testInput) == 1)
+
+    val input = readInput("day20/Day20")
+//    part1(input).println()
+//    part2(input).println()
+}
